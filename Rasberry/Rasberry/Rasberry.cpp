9bee4d6329cd958a43fcd8e3bfb686ba19e66304
Oliver-Dahlen <oliverdahlen@gmail.com>
2022-03-19 16:23:33 +0100

    -

---
+++ Rasberry/Rasberry/Rasberry.cpp
@@ -0,0 +1,12 @@
+﻿// Rasberry.cpp : Defines the entry point for the application.
+//
+
+#include "Rasberry.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
